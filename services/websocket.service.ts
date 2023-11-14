62c68e112b6888151a0c1fe19b94c815befdffaa
flvsantos15 <flvsantos300@gmail.com>
2023-11-14 11:58:15 -0300

    chore: tools update

--- services/websocket.service.ts
+++ services/websocket.service.ts
@@ -1,46 +1,42 @@
-import { Injectable } from '@angular/core';
-import { webSocket } from 'rxjs/webSocket';
+import { Injectable } from '@angular/core'
+import { webSocket } from 'rxjs/webSocket'
 // import { ChatroomService } from 'src/modules/chatroom/services/chatroom.service';
 // import { PlayroomService } from './playroom.service';
-import { LibsService } from 'src/modules/tools/services/libs.service';
+import { LibsService } from 'src/modules/tools/services/libs.service'
 
-import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router'
 
 export interface WebSocketsConfig {
-  pathname: string;
-  port: number;
-  service?: any;
-  wsService?: any;
-  wsStream?: any;
-};
+  pathname: string
+  port: number
+  service?: any
+  wsService?: any
+  wsStream?: any
+}
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class WebsocketService {
-
   /**
-   * @description: 
-   */
-  public subject: any = undefined;
-
-  /**
-   * @description: 
-   */
-  public consumerData: any = {};
-
-  /** 
    * @description:
    */
-  private websocket_config: Array<WebSocketsConfig> = [];
+  public subject: any = undefined
 
   /**
-   * @description: 
+   * @description:
    */
-  constructor(
-    private router: Router,
-    private l: LibsService,
-  ) { }
+  public consumerData: any = {}
+
+  /**
+   * @description:
+   */
+  private websocket_config: Array<WebSocketsConfig> = []
+
+  /**
+   * @description:
+   */
+  constructor(private router: Router, private l: LibsService) {}
 
   /**
    * @description: Injection des elements essentielles a la configuration du websocket.
@@ -57,24 +53,24 @@ export class WebsocketService {
     // for (let i = 0; i < config.length; i++) {
     //   this.websocket_config[i].service = config[i].service;
     //   this.websocket_config[i].wsService = this;
-    // } 
+    // }
   }
 
   /*
    * @description: Ici ont souscrit au différents éléments à envoyer et recevoir.
    */
   public connection(ws_config: WebSocketsConfig): void {
-    console.log(ws_config);
+    console.log('connection', 'websocket', ws_config)
     // alert (this.l.getCookie('sessionid'));
-    const url = this.getWebsocketUrl(ws_config);
-    ws_config.wsStream = webSocket(url);
+    const url = this.getWebsocketUrl(ws_config)
+    ws_config.wsStream = webSocket(url)
     ws_config.wsStream.subscribe(
       (msg: any) => '', // Called whenever there is a message from the server.
       (err: any) => this.error_traitement(ws_config, err), // Called if at any point WebSocket API signals some kind of error.
       () => console.log('complete') // Called when connection is closed (for whatever reason).
-    );
-    this.receive(ws_config);
-    (window as any).websocket = ws_config.wsStream;
+    )
+    this.receive(ws_config)
+    ;(window as any).websocket = ws_config.wsStream
     // this.change_page();
   }
 
@@ -82,20 +78,21 @@ export class WebsocketService {
    * @description: Il serait important par la suite de créer plusieurs possibilité de shéma.
    */
   public receive(ws_config: WebSocketsConfig): void {
-    ws_config.service.ws_connection = ws_config;
+    ws_config.service.ws_connection = ws_config
 
     try {
       ws_config.wsStream.asObservable().subscribe((msg: any) => {
         for (let key of Object.keys(msg)) {
-          const recept_key = `recept__${key}`;
-          let recept__function: Function | undefined = ws_config.service[recept_key];
+          const recept_key = `recept__${key}`
+          let recept__function: Function | undefined =
+            ws_config.service[recept_key]
           if (recept__function === undefined) {
-            alert (`recept__${key} is not defined`);
-            return; 
+            alert(`recept__${key} is not defined`)
+            return
           }
-          ws_config.service[recept_key](msg[key]);
+          ws_config.service[recept_key](msg[key])
         }
-      });
+      })
     } catch (e) {}
   }
 
@@ -104,48 +101,48 @@ export class WebsocketService {
    * x. Tenter à la fin de reconnecter le flux.
    */
   public error_traitement(ws_config: WebSocketsConfig, err: any): void {
-    console.error('error_traitement'.repeat(1));
+    console.error('error_traitement'.repeat(1))
     setTimeout(() => {
-      this.connection(ws_config);
-    }, 1000);
+      this.connection(ws_config)
+    }, 1000)
   }
 
   /**
-   * @description: 
+   * @description:
    */
   private getWebsocketUrl(ws_config: WebSocketsConfig): string {
-    return  `${this.get_protocol()}${this.l.get_host()}:${ws_config.port}/${ws_config.pathname}`;
+    return `${this.get_protocol()}${this.l.get_host()}:${ws_config.port}/${
+      ws_config.pathname
+    }`
   }
 
   /**
-   * @description: 
+   * @description:
    */
   private get_protocol(): string {
     if (window.location.protocol === 'https:') {
-      return 'ws://';
+      return 'ws://'
     }
-    return 'ws://';
+    return 'ws://'
   }
 
   /**
-   * @description: 
+   * @description:
    */
-  public send(
-      ws_connection: WebSocketsConfig | undefined, 
-      params: any): void {
+  public send(ws_connection: WebSocketsConfig | undefined, params: any): void {
     if (ws_connection === undefined) {
-      return;
+      return
     }
-    ws_connection.wsStream.next(params);
+    ws_connection.wsStream.next(params)
   }
 
   /**
    * @description: Add the websocket config to the list of websocket config.
    * and run the connection.
-  */
+   */
   public setConfig(ws_config: WebSocketsConfig): void {
-    this.websocket_config.push(ws_config);
-    ws_config.wsService = this;
-    this.connection(ws_config);    
+    this.websocket_config.push(ws_config)
+    ws_config.wsService = this
+    this.connection(ws_config)
   }
 }
